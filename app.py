c60103169bf343b75a05fe6b4f2231a71a02eee6
Simon Hamelin <shamelin941@gmail.com>
2019-09-30 13:04:12 -0400

    Basic bot

--- app.py
+++ app.py
@@ -5,13 +5,27 @@
 
 import discord, yaml
 
+client = discord.Client()
+
 class Lucio(object):
     def __init__(self):
         self.load_config()
-        print(self.config["hello"])
+        client.run('NjI4MjIyNzM3MzY1NTk4MjI5.XZII7w.iY6-mtpPk3L-zhHSZQZrKk2MvD8')
 
     def load_config(self):
         self.file = open("config.txt")
         self.config = yaml.load(self.file, Loader=yaml.FullLoader)
+    
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 Lucio()
